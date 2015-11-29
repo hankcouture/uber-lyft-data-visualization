08a69606fb3bae14558d08ac9ff417b095aa6b14
Hank Couture <hankcouture@gmail.com>
2015-11-29 14:31:34 -0600

    Added Graph on Ride Cost and Started Tab Functionality

--- app.js
+++ app.js
@@ -13,156 +13,174 @@ d3.selection.prototype.hide = function() {
 
 
 // Grab data from data.js file
-var data = data.rideData.data;
-var length = 0;
+// var data = data.rideData.numberOfRides;
 
-// Set our margins
-var margin = {
-    top: 20,
-    right: 20,
-    bottom: 30,
-    left: 60
-},
-width = 1000 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+function makeBarChart(data) {
 
-// Our X scale
-var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1);
+    // Set our margins
+    var margin = {
+        top: 20,
+        right: 20,
+        bottom: 30,
+        left: 60
+    },
+    width = 1000 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
-// Our Y scale
-var y = d3.scale.linear()
-    .rangeRound([height, 0]);
+    // Our X scale
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1);
 
-// Our color bands
-var color = d3.scale.ordinal()
-    .range(["#1a1a1a", "#474747", "#EA0B8C", "#a30762"]);
+    // Our Y scale
+    var y = d3.scale.linear()
+        .rangeRound([height, 0]);
 
-// Use our X scale to set a bottom axis
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+    // Our color bands
+    var color = d3.scale.ordinal()
+        .range(["#1a1a1a", "#474747", "#EA0B8C", "#a30762"]);
 
-// Same for our left axis
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
+    // Use our X scale to set a bottom axis
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
 
-// Add our chart to the #chart div
-var svg = d3.select(".graph").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    // Same for our left axis
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+
+    // Add our chart to the #chart div
+    var svg = d3.select(".graph").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-// Map our columns to our colors
-color.domain(d3.keys(data[0]).filter(function (key) {
-    return key !== "Date";
-}));
+    // Map our columns to our colors
+    color.domain(d3.keys(data[0]).filter(function (key) {
+        return key !== "Date";
+    }));
 
-data.forEach(function (d) {
-    var y0 = 0;
-    d.types = color.domain().map(function (name) {
-        return {
-            name: name,
-            y0: y0,
-            y1: y0 += +d[name]
-        };
+    data.forEach(function (d) {
+        var y0 = 0;
+        d.types = color.domain().map(function (name) {
+            return {
+                name: name,
+                y0: y0,
+                y1: y0 += +d[name]
+            };
+        });
+        d.total = d.types[d.types.length - 1].y1;
     });
-    d.total = d.types[d.types.length - 1].y1;
-});
 
-// Our X domain is our set of years
-x.domain(data.map(function (d) {
-    return d.Date;
-}));
+    // Our X domain is our set of years
+    x.domain(data.map(function (d) {
+        return d.Date;
+    }));
 
-// Our Y domain is from zero to our highest total
-y.domain([0, d3.max(data, function (d) {
-    return d.total;
-})]);
+    // Our Y domain is from zero to our highest total
+    y.domain([0, d3.max(data, function (d) {
+        return d.total;
+    })]);
 
-svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
 
-svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis);
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
 
 
-var month = svg.selectAll(".Date")
-    .data(data)
-    .enter().append("g")
-    .attr("class", "g")
-    .attr("transform", function (d) {
-    return "translate(" + x(d.Date) + ",0)";
-});
+    var month = svg.selectAll(".Date")
+        .data(data)
+        .enter().append("g")
+        .attr("class", "g")
+        .attr("transform", function (d) {
+        return "translate(" + x(d.Date) + ",0)";
+    });
 
-function makeBars() {
-    return month.selectAll("rect")
-        .data(function (d) {
-        return d.types;
-    })
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("width", function(d) {
-         return x.rangeBand();   
+    function makeBars() {
+        return month.selectAll("rect")
+            .data(function (d) {
+            return d.types;
         })
-        .attr("y", function (d) {
-        return y(d.y1);
-    })
-        .attr("height", function (d) {
-        return y(d.y0) - y(d.y1);
-    })
-        .style("fill", function (d) {
-        return color(d.name);
-    })
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("width", function(d) {
+             return x.rangeBand();   
+            })
+            .attr("y", function (d) {
+            return y(d.y1);
+        })
+            .attr("height", function (d) {
+            return y(d.y0) - y(d.y1);
+        })
+            .style("fill", function (d) {
+            return color(d.name);
+        })
+            // .text(function(d) {
+            //     console.log(d)
+            //     return d.y1-d.y0;
+            // })
+    }
+
+
+    // Bar graph animation
+    function intro(d) {
+      d.hide()
+      var original = d.attr('y');
+      d.attr('y', height + 100)
+      var t = 100;
+      d.transition()
+         .delay(t + (count*10))
+         .duration(t * 5)
+         .attr('y', original)
+      d.show();
+    }
+
+    var bar = makeBars();
+    var count = 0;
+    bar.each(function(d) {
+        intro(d3.select(this))
+        count++;
+    });
+
+    // Bar Graph Legend
+    var legend = svg.selectAll(".legend")
+        .data(color.domain().slice().reverse())
+        .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function (d, i) {
+        return "translate(0," + i * 20 + ")";
+    });
+
+    legend.append("rect")
+        .attr("x", width - (width - 118))
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", color);
+
+    legend.append("text")
+        .attr("x", width - (width - 100))
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text(function (d) {
+        return d;
+    });
+  
 }
 
-// Bar graph animation
-function intro(d) {
-  d.hide()
-  var original = d.attr('y');
-  d.attr('y', height + 100)
-  var t = 100;
-  d.transition()
-     .delay(t + (count*10))
-     .duration(t * 5)
-     .attr('y', original)
-  d.show();
-}
+$('.numberOfRides').on('click', function(){
+  $('.graph').empty();
+  makeBarChart(data.rideData.numberOfRides);
+})
 
-var bar = makeBars();
-var count = 0;
-bar.each(function(d) {
-    intro(d3.select(this))
-    count++;
-});
+$('.rideCost').on('click', function(){
+  $('.graph').empty();
+  makeBarChart(data.rideData.rideCost);
+})
 
-
-// Bar Graph Legend
-var legend = svg.selectAll(".legend")
-    .data(color.domain().slice().reverse())
-    .enter().append("g")
-    .attr("class", "legend")
-    .attr("transform", function (d, i) {
-    return "translate(0," + i * 20 + ")";
-});
-
-legend.append("rect")
-    .attr("x", width - (width - 118))
-    .attr("width", 18)
-    .attr("height", 18)
-    .style("fill", color);
-
-legend.append("text")
-    .attr("x", width - (width - 100))
-    .attr("y", 9)
-    .attr("dy", ".35em")
-    .style("text-anchor", "end")
-    .text(function (d) {
-    return d;
-});
--- data.js
+++ data.js
@@ -2,7 +2,7 @@ var data = {
 	rideData:{
 		description: 'Uber vs. Lyft Ride Data',
 		column_names:["Month", "UberX", "Uber Pool", "Lyft", "Lyft Line"],
-		data:[
+		numberOfRides:[
 			{
 				'Date': 1,
 				'UberX': 1,
@@ -135,6 +135,141 @@ var data = {
 				'Uber Pool': 0,
 				'Lyft': 1,
 				'Lyft Line': 2
+			}
+		],
+		rideCost:[
+			{
+				'Date': 1,
+				'UberX': 17.25,
+				'Uber Pool': 0,
+				'Lyft': 0,
+				'Lyft Line': 0
+			},
+			{
+				'Date': 2,
+				'UberX': 0,
+				'Uber Pool': 0,
+				'Lyft': 0,
+				'Lyft Line': 0
+			},
+			{
+				'Date': 3,
+				'UberX': 48.07,
+				'Uber Pool': 0,
+				'Lyft': 0,
+				'Lyft Line': 0
+			},
+			{
+				'Date': 4,
+				'UberX': 72.88,
+				'Uber Pool': 0,
+				'Lyft': 0,
+				'Lyft Line': 0
+			},
+			{
+				'Date': 5,
+				'UberX': 46.05,
+				'Uber Pool': 0,
+				'Lyft': 0,
+				'Lyft Line': 0
+			},
+			{
+				'Date': 6,
+				'UberX': 10.93,
+				'Uber Pool': 0,
+				'Lyft': 0,
+				'Lyft Line': 0
+			},
+			{
+				'Date': 7,
+				'UberX': 60.00,
+				'Uber Pool': 0,
+				'Lyft': 0,
+				'Lyft Line': 0
+			},
+			{
+				'Date': 8,
+				'UberX': 0,
+				'Uber Pool': 0,
+				'Lyft': 0,
+				'Lyft Line': 0
+			},
+			{
+				'Date': 9,
+				'UberX': 18.25,
+				'Uber Pool': 73.04,
+				'Lyft': 16.43,
+				'Lyft Line': 0
+			},
+			{
+				'Date': 10,
+				'UberX': 119.88,
+				'Uber Pool': 0,
+				'Lyft': 2.66,
+				'Lyft Line': 0
+			},
+			{
+				'Date': 11,
+				'UberX': 61.47,
+				'Uber Pool': 0,
+				'Lyft': 0,
+				'Lyft Line': 0
+			},
+			{
+				'Date': 12,
+				'UberX': 50.35,
+				'Uber Pool': 0,
+				'Lyft': 0,
+				'Lyft Line': 0
+			},
+			{
+				'Date': 13,
+				'UberX': 70.25,
+				'Uber Pool': 43.80,
+				'Lyft': 0,
+				'Lyft Line': 0
+			},
+			{
+				'Date': 14,
+				'UberX': 65.56,
+				'Uber Pool': 0,
+				'Lyft': 0,
+				'Lyft Line': 0
+			},
+			{
+				'Date': 15,
+				'UberX': 46.44,
+				'Uber Pool': 11.86,
+				'Lyft': 0,
+				'Lyft Line': 0
+			},
+			{
+				'Date': 16,
+				'UberX': 27.27,
+				'Uber Pool': 0,
+				'Lyft': 0,
+				'Lyft Line': 0
+			},
+			{
+				'Date': 17,
+				'UberX': 0,
+				'Uber Pool': 0,
+				'Lyft': 51.03,
+				'Lyft Line': 0
+			},
+			{
+				'Date': 18,
+				'UberX': 13.40,
+				'Uber Pool': 23.87,
+				'Lyft': 70.39,
+				'Lyft Line': 2.00
+			},
+			{
+				'Date': 19,
+				'UberX': 54.49,
+				'Uber Pool': 0,
+				'Lyft': 10.02,
+				'Lyft Line': 19.82
 			}
 		],
 	}
